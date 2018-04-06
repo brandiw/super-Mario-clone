3da1d5e7f68b5483b6a4d31f0e8f309c935b4765
thegreatsantini <lss13@my.fsu.edu>
2018-04-05 20:49:55 -0700

    completed vid 2 sprites and layers

---
+++ public/js/Compositor.js
@@ -0,0 +1,13 @@
+// draw all layers in order
+export default class Compositor {
+    constructor() {
+      this.layers = [];
+    }
+  
+    draw(context) {
+      // layer is a function that draws on a context contains all the info to draw itself
+      this.layers.forEach(layer => {
+        layer(context);
+      });
+    }
+  }
--- public/js/app.js
+++ public/js/app.js
@@ -1,47 +1,47 @@
-import SpriteSheet from "./SpriteSheet.js";
+import Compositor from "./Compositor.js";
 import { loadLevel } from "./loaders.js";
-import { loadMarioSprite, loadBackgroundSprites } from './sprites.js'
-
+import { loadMarioSprite, loadBackgroundSprites } from "./sprites.js";
+import { createBackgroundLayer } from "./layers.js";
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
-function drawBackground(background, context, sprites) {
-  background.ranges.forEach(([x1, x2, y1, y2]) => {
-
-    for (let x = x1; x < x2; x++) {
-      for (let y = y1; y < y2; y++) {
-        // loop through tiles to draw 
-        sprites.drawTile(background.tile, context, x, y);
-      }
+function createSpriteLayer(sprite, pos) {
+  return function drawSpriteLayer(context) {
+    for (let i = 0; i < 20; i++) {
+      sprite.draw("idle", context, pos.x + i * 16, pos.y);
     }
-  });
+  };
 }
 
 // loading two functions in parallel, would like to review this more in depth
 Promise.all([
   loadMarioSprite(),
   loadBackgroundSprites(),
-  loadLevel('1-1')
-]).
-  then(([marioSprite, sprites, level]) => {
-    console.log(level)
-    level.backgrounds.forEach(background => {
-      drawBackground(background, context, sprites)
-    })
+  loadLevel("1-1")
+]).then(([marioSprite, backgroundSprites, level]) => {
+  const comp = new Compositor();
 
-    const pos = {
-      x: 64,
-      y: 64
-    };
+  const backgroundLayer = createBackgroundLayer(
+    level.backgrounds,
+    backgroundSprites
+  );
+  comp.layers.push(backgroundLayer);
 
-    function update() {
-      marioSprite.draw('idle', context, pos.x, pos.y)
-      pos.x += 2
-      pos.y += 2
-      requestAnimationFrame(update)
-    }
+  const pos = {
+    x: 0,
+    y: 0
+  };
 
-    // update()
+  const spriteLayer = createSpriteLayer(marioSprite, pos);
+  comp.layers.push(spriteLayer);
 
-  });
+  function update() {
+    comp.draw(context);
+    pos.x += 2;
+    pos.y += 2;
+    requestAnimationFrame(update);
+  }
+
+    // update();
+});
